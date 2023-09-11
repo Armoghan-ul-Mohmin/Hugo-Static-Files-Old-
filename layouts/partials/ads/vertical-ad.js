5c35cfc6090f0a195b746c9dc43dd9e2d310b2fe
Armoghan <Armoghan16@gmail.com>
2023-09-11 15:24:45 +0500

    Deleted Site

--- layouts/partials/ads/vertical-ad.js
+++
@@ -1,3 +0,0 @@
- async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2047325115998150"
-     crossorigin="anonymous">
-    (adsbygoogle = window.adsbygoogle || []).push({});
